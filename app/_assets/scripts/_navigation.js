d705d338e793e4f8eac19e2617bba22adfbeff2b
Rene Dellefont <renedellefont@gmail.com>
2016-03-06 23:07:52 -0500

    Add header shrink functionality
    - BEM the header
    - change header layout from flex to float
    - strip javascript to just include shrink

--- app/_assets/scripts/_navigation.js
+++ app/_assets/scripts/_navigation.js
@@ -2,15 +2,38 @@
 var $ = require('jquery');
 
 $(document).ready(function() {
-  var menuToggle = $('#js-mobile-menu').unbind();
-  $('#js-navigation-menu').removeClass("show");
+  var header = $('header[role=banner]');
 
-  menuToggle.on('click', function(e) {
-    e.preventDefault();
-    $('#js-navigation-menu').slideToggle(function(){
-      if($('#js-navigation-menu').is(':hidden')) {
-        $('#js-navigation-menu').removeAttr('style');
+  $(window).scroll(function() {
+    var stickDistance = 0;
+    var scroll = $(window).scrollTop();
+
+    if ( scroll >= 40 ) {
+      header.addClass('smaller');
+      /*$('body').addClass('shrink');*/
+    } else {
+      header.removeClass('smaller');
+      /*$('body').removeClass('shrink');*/
+    }
+
+    /*
+
+    // when scroll distance > 0 (stickDistance)
+    if ($(window).scrollTop() > stickDistance) {
+      // have "fixed" position classes been added?
+      if (!header.hasClass('header--fixed')) {
+        header.addClass('header--fixed');
+        $('body').addClass('body--fixed');
       }
-    });
+    // when scroll distance is back to top
+    } else if (scroll <= stickDistance) {
+      // have "fixed" position classes been removed?
+      if (header.hasClass('header--fixed')) {
+        header.removeClass('header--fixed');
+        $('body').removeClass('body--fixed');
+      }
+    }
+    */
+
   });
 });
