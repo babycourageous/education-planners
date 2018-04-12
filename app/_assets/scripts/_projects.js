baf8923c8b083646e300858ac57d1f48a380cb62
Rene Dellefont <renedellefont@gmail.com>
2018-04-12 12:01:18 -0400

    Update portfolio
    Remove jQuery dependency for vanilla JS options.

--- app/_assets/scripts/_projects.js
+++ app/_assets/scripts/_projects.js
@@ -1,50 +1,78 @@
-'use strict';
-var $ = require('jquery');
-var slick = require('slick-carousel');
-require('magnific-popup');
+import vex from "vex-js";
+import Flickity from "flickity";
 
-$(document).ready(function() {
-  $('.project__link').magnificPopup({
-    type:'inline',
-    midClick: true,
-    //closeBtnInside: false,
-    callbacks: {
-      open: function() {
-        this.content.find('.project__carousel').slick({
-          setPosition: 0,
-          autoplay: false,
-          pauseOnHover: true,
-          dots: true,
-          speed: 500,
-          arrows: false,
-          dotsClass: 'project__dots',
-        })
-      },
-      close: function() {
-        this.content.find('.project__carousel').slick('unslick');
-      }
+const links = document.querySelectorAll("#js-modal-open");
+links.forEach(item => {
+  const content = document.querySelector(item.getAttribute("href"));
+  const carousel = content.querySelector(".c-project__carousel");
+
+  item.addEventListener("click", function(e) {
+    e.preventDefault();
+    const v = vex.open({
+      unsafeContent: content.outerHTML
+      //className: "vex-theme-wireframe" // Overwrites defaultOptions
+    });
+
+    // vanilla JS
+    var cells = carousel.querySelectorAll(".c-project__item");
+    var hasMultipleCells = cells && cells.length > 1;
+    // init Flickity if mulitple cells
+    if (hasMultipleCells) {
+      const f = new Flickity(
+        v.contentEl.querySelector(".c-project__carousel"),
+        {
+          // options
+          contain: true,
+          prevNextButtons: false,
+          pageDots: true
+        }
+      );
+
+      f.resize();
     }
-    /*callbacks: {
-      open: function() {
-        // Will fire when this exact popup is opened
-        // this - is Magnific Popup object
-        console.log(this.content);
-        //console.log(this.st.el);
-
-        this.content.slick({
-          setPosition: 0,
-          autoplay: false,
-          pauseOnHover: true,
-          dots: true,
-          speed: 500,
-          arrows: false,
-          dotsClass: 'project__dots',
-        })
-      },
-      close: function() {
-        this.content.slick('unslick');
-      }
-    }*/
   });
-
 });
+
+/*
+// instanciate new modal
+var modal = new tingle.modal({
+    footer: true,
+    stickyFooter: false,
+    closeMethods: ['overlay', 'button', 'escape'],
+    closeLabel: "Close",
+    cssClass: ['custom-class-1', 'custom-class-2'],
+    onOpen: function() {
+        console.log('modal open');
+    },
+    onClose: function() {
+        console.log('modal closed');
+    },
+    beforeClose: function() {
+        // here's goes some logic
+        // e.g. save content before closing the modal
+        return true; // close the modal
+        return false; // nothing happens
+    }
+});
+
+// set content
+modal.setContent('<h1>here\'s some content</h1>');
+
+// add a button
+modal.addFooterBtn('Button label', 'tingle-btn tingle-btn--primary', function() {
+    // here goes some logic
+    modal.close();
+});
+
+// add another button
+modal.addFooterBtn('Dangerous action !', 'tingle-btn tingle-btn--danger', function() {
+    // here goes some logic
+    modal.close();
+});
+
+// open modal
+modal.open();
+
+// close modal
+modal.close();
+*/
