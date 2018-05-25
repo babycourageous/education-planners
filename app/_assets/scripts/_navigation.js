ac54e7df872bc6c9502e5f53e78aa544db248d18
Unknown <renedellefont@gmail.com>
2018-05-24 22:35:28 -0400

    Refactor DOM reference declarations

--- app/_assets/scripts/_navigation.js
+++ app/_assets/scripts/_navigation.js
@@ -4,17 +4,17 @@ import SmoothScroll from "smooth-scroll";
 const header = document.querySelector("header[role=banner]");
 const nav = document.querySelector("nav");
 
+// reference to nav ul
+const topMenu = document.querySelector("#js-navigation-menu");
+// All list link items
+const menuItems = topMenu.querySelectorAll("a");
+const menuToggle = document.querySelector('#js-mobile-menu');
+
 // navigation scrolling and highlighting variables
 let lastId;
 
-// reference to nav ul
-const topMenu = document.querySelector("#js-navigation-menu");
-
 // use 60px base instead of calculating on load because of shrinking nav
 const topMenuHeight = 59; //topMenu.outerHeight(),
-
-// All list link items
-const menuItems = topMenu.querySelectorAll("a");
 
 // Bind to scroll
 window.addEventListener("scroll", function() {
