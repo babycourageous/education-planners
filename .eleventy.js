df31f733717a6eaf22c35f525f5796a7c8550cf1
Rene Dellefont <renedellefont@gmail.com>
2019-03-05 10:29:37 -0500

    Update files, packages, data, includes to 11ty

---
+++ .eleventy.js
@@ -0,0 +1,40 @@
+const marked = require('marked')
+
+module.exports = function(eleventyConfig) {
+  // Aliases are in relation to the _includes folder
+  eleventyConfig.addLayoutAlias('default', 'layouts/default.njk')
+  eleventyConfig.addLayoutAlias('project', 'layouts/project.njk')
+  eleventyConfig.addLayoutAlias('resume', 'layouts/resume.njk')
+
+  eleventyConfig.addFilter('md', function(value) {
+    let result
+    try {
+      result = marked(value).trim()
+      //result = result.replace(/^<p>|<\/p>$/g, '')
+      return result
+    } catch (e) {
+      console.error('Error processing markdown:', e)
+      return value
+    }
+  })
+
+  // {{ array | where: key,value }}
+  eleventyConfig.addFilter('where', function(array, key, value) {
+    return array.filter(item => item.data[key] === value)
+  })
+
+  eleventyConfig.addCollection('team', collection => {
+    const stuff = collection.getFilteredByGlob('_team/*.md')
+    return stuff
+  })
+
+  //eleventyConfig.addPassthroughCopy('admin')
+
+  return {
+    dir: {
+      input: './', // Equivalent to Jekyll's source property
+      output: './_site', // Equivalent to Jekyll's destination property
+    },
+    htmlTemplateEngine: 'njk',
+  }
+}
--- _assets/javascript/_projects.js
+++ _assets/javascript/_projects.js
@@ -1,49 +1,50 @@
-import Dialog from "a11y-dialog-component";
-import Flickity from "flickity";
+import Dialog from 'a11y-dialog-component'
+import Flickity from 'flickity'
 
-if (document.getElementById("work")) {
-  const portfolio = document.querySelector("#js-portfolio");
-  const links = portfolio.querySelectorAll("a[data-target]");
-  const carousels = portfolio.querySelectorAll(".js-project-carousel");
-  const modals = document.querySelectorAll(".c-modal");
+console.log('PROJECTS')
+if (document.getElementById('work')) {
+  const portfolio = document.querySelector('#js-portfolio')
+  const links = portfolio.querySelectorAll('a[data-target]')
+  const carousels = portfolio.querySelectorAll('.js-project-carousel')
+  const modals = document.querySelectorAll('.c-modal')
 
   for (var i = 0, len = carousels.length; i < len; i++) {
-    const galleryElem = carousels[i];
+    const galleryElem = carousels[i]
     new Flickity(galleryElem, {
       // options
       contain: true,
-      prevNextButtons: false
+      prevNextButtons: false,
       //     pageDots: true,
-    });
+    })
   }
 
-  let currentProject;
-  let currentProjectHome;
+  let currentProject
+  let currentProjectHome
 
   links.forEach(link => {
-    const targetID = link.dataset.target;
+    const targetID = link.dataset.target
 
     const d = new Dialog(`#${targetID}`, {
-      closingSelector: ".js-dialog-close",
+      closingSelector: '.js-dialog-close',
       backdropSelector: `#${targetID}`,
-      labelledbyId: `${targetID}-title`
-    });
+      labelledbyId: `${targetID}-title`,
+    })
 
     modals.forEach(modal => {
-      modal.classList.remove("invisible");
-    });
+      modal.classList.remove('invisible')
+    })
 
-    link.addEventListener("click", function(e) {
-      e.preventDefault();
+    link.addEventListener('click', function(e) {
+      e.preventDefault()
 
-      currentProjectHome = e.currentTarget.parentNode.parentNode;
-      currentProject = portfolio.querySelector(`#${targetID}`);
+      currentProjectHome = e.currentTarget.parentNode.parentNode
+      currentProject = portfolio.querySelector(`#${targetID}`)
 
-      const carousel = currentProject.querySelector(".js-project-carousel");
-      const cells = carousel.querySelectorAll(".js-project-item");
-      const hasMultipleCells = cells && cells.length > 1;
+      const carousel = currentProject.querySelector('.js-project-carousel')
+      const cells = carousel.querySelectorAll('.js-project-item')
+      const hasMultipleCells = cells && cells.length > 1
 
-      d.open();
-    });
-  });
+      d.open()
+    })
+  })
 }
