54dc65ef20138ced2e1ea7aa06ee9b561a8e0f41
Rene Dellefont <renedellefont@gmail.com>
2019-07-18 18:52:55 -0400

    Add lickity to whitlist for purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -6,6 +6,7 @@ const cssnano = require('cssnano')
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: ['./src/**/*.html', './src/**/*.njk'],
+  whitelistPatterns: [/^flickity/],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
