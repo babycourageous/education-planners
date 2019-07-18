bf7bbfbb25bdf86144f8558e46058c9faacb0c0e
Rene Dellefont <renedellefont@gmail.com>
2019-07-18 19:02:48 -0400

    Add flickity children to purgeCSS whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -7,6 +7,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: ['./src/**/*.html', './src/**/*.njk'],
   whitelistPatterns: [/^flickity/],
+  whitelistPatternsChildren: [/^flickity/],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
