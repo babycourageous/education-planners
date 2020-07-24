9774cfa8d2661729e12f8e76c30072ee77519b15
Rene Dellefont <renedellefont@gmail.com>
2020-07-24 12:29:22 -0400

    Update default extractor per TW docs

--- postcss.config.js
+++ postcss.config.js
@@ -10,7 +10,15 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   whitelistPatternsChildren: [/^flickity/],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  defaultExtractor: (content) => {
+    // Capture as liberally as possible, including things like `h-(screen-1.5)`
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+
+    return broadMatches.concat(innerMatches)
+  },
 })
 
 module.exports = {
