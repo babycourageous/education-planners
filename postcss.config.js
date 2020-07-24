9a94bae278e8bea6c29cadac9c359557426f522c
Rene Dellefont <renedellefont@gmail.com>
2020-07-24 12:47:43 -0400

    Use manaul purge settings per tw docs

--- postcss.config.js
+++ postcss.config.js
@@ -11,15 +11,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   whitelistPatternsChildren: [/^flickity/],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: (content) => {
-    // Capture as liberally as possible, including things like `h-(screen-1.5)`
-    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-
-    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
-
-    return broadMatches.concat(innerMatches)
-  },
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
 })
 
 module.exports = {
